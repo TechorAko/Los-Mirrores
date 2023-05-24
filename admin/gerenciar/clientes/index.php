46a6daa74e91636e6a0a8d58e629777e891709a8
TechorAko <gabrielcavalhiere1@outlook.com>
2023-05-24 20:04:07 -0300

    Faltando algumas pastas ainda em branco.

---
+++ admin/gerenciar/clientes/index.php
@@ -0,0 +1,21 @@
+<?php
+
+require_once "../../../bibliotecas/mysqli.bli";
+require_once "../../../bibliotecas/conecta_mysqli";
+require_once "../../ver_session";
+
+?>
+<html>
+    <head>
+        <title>Usuários - Los Mirrores (Administração)</title>
+        <link rel="stylesheet" href="../../style.css">
+        <meta charset="UTF-8">
+        <?php include '../../../bibliotecas/bootstrap.html'; ?>
+    </head>
+    <body>
+        <?php include '../../header.php'; ?>
+        <div class="container" style="margin: 25px;">
+            
+        </div>
+    </body>
+</html>
